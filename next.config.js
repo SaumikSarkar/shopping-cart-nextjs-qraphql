c91faa3db691df3a11f0efe3915827e7601ea9f8
Saumik Sarkar <sarkar.saumik15@gmail.com>
2025-07-31 19:08:56 +0530

    ci: deploy app to vercel
    Configures deploy.yml for deploying application to vercel

--- next.config.js
+++ next.config.js
@@ -1,24 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
-  experimental: {
-    serverActions: true,
-  },
   images: {
     remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
       {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
     ],
   },
-  basePath: "/shopping-cart-nextjs-qraphql",
-  assetPrefix: "/shopping-cart-nextjs-qraphql/",
 };
 
 export default nextConfig;
