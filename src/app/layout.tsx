a25fe5bbaddc094a98873c0611dd6639279be39e
Saumik Sarkar <sarkar.saumik15@gmail.com>
2025-07-31 21:03:32 +0530

    feat(app-meta): add meta information for the website
    Adds website meta information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
-"use client";
-
 import "@/styles/globals.css";
 import { CssBaseline } from "@mui/material";
+import { Metadata } from "next";
 
 import AppProvider from "./app-provider";
 
@@ -10,6 +9,16 @@ import Header from "@/components/common/Header/Header";
 type LayoutProps = Readonly<{
   children: React.ReactNode;
 }>;
+
+export const metadata: Metadata = {
+  title: "Shop Your Jewel",
+  description: "Next.js + GraphQL shopping cart",
+  icons: {
+    icon: "/assets/logo.png",
+    shortcut: "/assets/logo.png",
+    apple: "/assets/logo.png",
+  },
+};
 
 export default function Layout({ children }: LayoutProps) {
   return (
